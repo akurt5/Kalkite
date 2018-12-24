8c3e97dff83d4b711f15e9df732ac693da5f4a2f
Anton Kurtz <anton.n.kurtz@gmail.com>
2018-12-24 11:48:15 +1100

    Commit before i leave CBR. Pretty sure I have started reworking the terrain generation in order to add turning to the road.

--- Assets/Scripts/RoadSegment.cs
+++ Assets/Scripts/RoadSegment.cs
@@ -10,23 +10,31 @@ public class RoadSegment : Segment
     public static GrassSegment[] PrevGrass = new GrassSegment[2];
     public GameObject GrassSegPrefab;
     GrassSegment[] grass = new GrassSegment[2];
-    public float grassWidth = 5;
     void Start()
     {
 
         float PrevYL =  0;
-        int PrevVert = -1;
+        int PrevVertL = -1;
 
         if(PrevGrass[0] != null)
         {
             PrevYL = PrevGrass[0].Vertices[2].y;
-            PrevVert = 0;
+            PrevVertL = 0;
+        }
+
+        float PrevYR =  0;
+        int PrevVertR = -1;
+
+        if(PrevGrass[0] != null)
+        {
+            PrevYR = PrevGrass[1].Vertices[3].y;
+            PrevVertR = 1;
         }
 
         grass[0] = Instantiate(GrassSegPrefab, transform.position, transform.rotation, transform).GetComponent<GrassSegment>();
-        grass[0].CreateSegment(new Vector2(grassWidth, 5),new Vector3(-grassWidth, 0, 0), GrassSegment.GetHeightVal(), 2, PrevYL, PrevVert);
+        grass[0].CreateSegment(new Vector2(GrassSegment.grassWidth, 5),new Vector3(-GrassSegment.grassWidth, 0, 0), GrassSegment.GetHeightVal(), 2, PrevYL, PrevVertL);
         grass[1] = Instantiate(GrassSegPrefab, transform.position, transform.rotation, transform).GetComponent<GrassSegment>();
-        grass[1].CreateSegment(new Vector2(grassWidth, 5),new Vector3(Dimensions.x, 0, 0)/*, GrassSegment.GetHeightVal(), 3*/);
+        grass[1].CreateSegment(new Vector2(GrassSegment.grassWidth, 5),new Vector3(Dimensions.x, 0, 0), GrassSegment.GetHeightVal(true), 3, PrevYR, PrevVertR);
 
         grass[0].PrevGrass = PrevGrass[0];
         grass[1].PrevGrass = PrevGrass[1];
--- Assets/Scripts/TerrainMeshCreation.cs
+++ Assets/Scripts/TerrainMeshCreation.cs
@@ -2,14 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public static class TerrainMeshCreation 
+public static class TerrainMeshCreation
 {
-	public static Mesh CreateQuad(Vector2 _Dimensions)
+    public static Mesh CreateQuad(Vector2 _Dimensions)
     {
         return CreateQuad(_Dimensions, new Vector3(0, 0, 0));
     }
-	public static Mesh CreateQuad(Vector2 _Dimensions, Vector3 _Position)
-	{
+    public static Mesh CreateQuad(Vector2 _Dimensions, Vector3 _Position)
+    {
         Vector3[] outTemp = new Vector3[4];
         return CreateQuad(_Dimensions, _Position, out outTemp);
     }
@@ -17,25 +17,25 @@ public static class TerrainMeshCreation
     {
         return CreateQuad(_Dimensions, _Position, out _Vertices, 0, -1, 0, -1);
     }
-    public static Mesh CreateQuad(Vector2 _Dimensions, Vector3 _Position, out Vector3[] _Vertices,  float _CornerVertexY, int _Vertex, float _OldVertPos, int _OldVert)
-	{
+    public static Mesh CreateQuad(Vector2 _Dimensions, Vector3 _Position, out Vector3[] _Vertices, float _CornerVertexY, int _Vertex, float _OldVertPos, int _OldVert)
+    {
         Mesh mesh = new Mesh();
 
         Vector3[] vertices = {
-			new Vector3(0, 0, 0) + _Position,
-			new Vector3(_Dimensions.x, 0, 0) + _Position,
-			new Vector3(0, 0, _Dimensions.y) + _Position,
-			new Vector3(_Dimensions.x, 0, _Dimensions.y) + _Position,
-		};
-        if(_Vertex != -1)
+            new Vector3(0, 0, 0) + _Position,
+            new Vector3(_Dimensions.x, 0, 0) + _Position,
+            new Vector3(0, 0, _Dimensions.y) + _Position,
+            new Vector3(_Dimensions.x, 0, _Dimensions.y) + _Position,
+        };
+        if (_Vertex != -1)
         {
             //Debug.Log(_CornerVertexY);
             vertices[_Vertex] = new Vector3(vertices[_Vertex].x, _CornerVertexY, vertices[_Vertex].z);
         }
-        if(_OldVert != -1)
+        if (_OldVert != -1)
         {
             vertices[_OldVert] = new Vector3(vertices[_OldVert].x, _OldVertPos, vertices[_OldVert].z);
-            
+
         }
 
         _Vertices = vertices;
@@ -50,7 +50,7 @@ public static class TerrainMeshCreation
         2,
         3,
         1,
-		};
+        };
 
         mesh.triangles = tri;
 
@@ -64,7 +64,7 @@ public static class TerrainMeshCreation
         mesh.normals = normals;
 
         Vector2[] uv = {
-			new Vector2(0, 0),	//0
+            new Vector2(0, 0),	//0
 			new Vector2(0, 0),	//1
 			new Vector2(0, 0),	//2
 			new Vector2(0, 0),	//3
@@ -72,7 +72,76 @@ public static class TerrainMeshCreation
 
         mesh.uv = uv;
 
-		return mesh;
-	}
+        return mesh;
+    }
+
+    public static Mesh CreateQuad(Vector2 _Dimensions, Vector3 _Position, out Vector3[] _OutVertices, Vector3[] _InVertices)
+    {
+        Mesh mesh = new Mesh();
+
+        Vector3[] vertices = {
+            new Vector3(0, 0, 0) + _Position,
+            new Vector3(_Dimensions.x, 0, 0) + _Position,
+            new Vector3(0, 0, _Dimensions.y) + _Position,
+            new Vector3(_Dimensions.x, 0, _Dimensions.y) + _Position,
+        };
+
+        for (int i = 0; i < 4; i++)
+        {
+            if (_InVertices[i] != new Vector3(-1, -1, -1))
+            {
+                if(_InVertices[i].x == -1)
+                {
+                    _InVertices[i].x = vertices[i].x;
+                }
+                if(_InVertices[i].y == -1)
+                {
+                    _InVertices[i].y = vertices[i].y;
+                }
+                if(_InVertices[i].z == -1)
+                {
+                    _InVertices[i].z = vertices[i].z;
+                }
+                
+                vertices[i] = _InVertices[i];
+            }
+        }
+
+        _OutVertices = vertices;
+
+        mesh.vertices = vertices;
+
+        int[] tri = {
+        0,
+        2,
+        1,
+
+        2,
+        3,
+        1,
+        };
+
+        mesh.triangles = tri;
+
+        Vector3[] normals = {
+        -Vector3.forward,	//0
+        -Vector3.forward,	//1
+        -Vector3.forward,	//2
+        -Vector3.forward,	//3
+		};
+
+        mesh.normals = normals;
+
+        Vector2[] uv = {
+            new Vector2(0, 0),	//0
+			new Vector2(0, 0),	//1
+			new Vector2(0, 0),	//2
+			new Vector2(0, 0),	//3
+		};
+
+        mesh.uv = uv;
+
+        return mesh;
+    }
 
 }
